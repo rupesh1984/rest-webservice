e67d3221217da20d713cbc5de535111e4e96c977
Rupesh Dubey <rupeshdubey@urbanclap.com>
2016-08-10 18:06:59 +0530

    Changes to push code

---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+ * Created by rupesh on 10/08/16.
+ */
+'use strict';
+var rest = require('restler');
+var Promise = require('bluebird')
+
+var webService = function(url, data, timeout){
+  this.url = url;
+  this.data = data;
+  this.response = '';
+  this.timeout = timeout
+  this.get = Promise.method(function(){
+    rest.get(this.url)
+      .on('complete', function(response){
+        this.response = response;
+        return true;
+      })
+      .on('error', function(response){
+        this.response = response;
+        return false;
+      })
+      .on('fail', function(response){
+        this.response = response;
+        return false;
+      })
+  });
+  this.post = Promise.method(function(cb){
+    rest.post(this.url, this.data)
+      .on('complete', function(response){
+        this.response = response;
+        return true;
+      })
+      .on('error', function(response){
+        this.response = response;
+        return false;
+      })
+      .on('fail', function(response){
+        this.response = response;
+        return false;
+      })
+  });
+  this.put = Promise.method(function(){
+    rest.put(this.url, this.data)
+      .on('complete', function(response){
+        this.response = response;
+        return true;
+      })
+      .on('error', function(response){
+        this.response = response;
+        return false;
+      })
+      .on('fail', function(response){
+        this.response = response;
+        return false;
+      })
+  });
+
+}
+
+
+module.exports = webService;
